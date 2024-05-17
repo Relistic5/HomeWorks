f51814e5b7d4afc26ca17c873eec1465e904e14a
Relistic5 <s.nedviga@relistic.ru>
2024-05-17 22:31:10 +0300

    Динамическая типизация

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = 'Сергей'
+print(f'Мое имя: {name}')
+age = 36
+print(f'Мой возраст: {age}')
+age = 36 + 1
+print(f'Через год мне стукнет: {age}')
+is_student = True
+print(f'Но я все уще учусь): {is_student}')
